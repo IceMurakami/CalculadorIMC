f30110d140d947d6fb8d9a07f8adf1b95919c7da
IceMurakami <estudosdafacularthur@gmail.com>
2023-03-04 19:33:23 -0300

    Alterando a logica do imc

--- js/appimc.js
+++ js/appimc.js
@@ -62,15 +62,26 @@ function validImc(imc){
     const nivel = ['Abaixo do peso','Peso Normal','Sobrepeso'
     ,'Obesidade grau 1','Obesidade grau 2', 'obesidade grau 3']
 
-    if(imc >= 39) return nivel[4];
-    
-    if(imc >= 34) return nivel[3];
-    
-    if(imc >= 24) return nivel[1];
 
-    if(imc >= 29.9) return nivel[2];
+    if(imc >= 39 ) return nivel[5];
 
-    if(imc >= 0 || imc >= 18) return nivel[0];
+    if(imc <= 18.5) return nivel[0];
+    
+    if(imc >= 18.5 && imc <= 24.9) return nivel[1];
+
+    if(imc >= 25 && imc <= 29.9) return nivel[2];
+    
+    if(imc >= 30 && imc <= 34) return nivel[3];
+    
+    if(imc >= 35 && imc<= 39.9 ) return nivel[4];
+    
+    
+    
+    
+
+    
+
+   
   
 
 }
